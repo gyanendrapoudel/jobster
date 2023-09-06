fd8d9127af1247db9f2567490d33e6efbb110469
Gyanendra <gyanen56@gmail.com>
2023-09-06 14:36:37 +1000

    setting up demo

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -53,13 +53,15 @@ const Register = () => {
     <Wrapper className="full-page">
       <form onSubmit={onSubmit} className="form">
         <Logo />
-        <h3>{values.isMember?'Login':'Register'}</h3>
-        {!values.isMember&&<FormRow
-          type="text"
-          name="name"
-          value={values.name}
-          handleChange={handleChange}
-        />}
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
         {/* email field */}
         <FormRow
           type="email"
@@ -76,11 +78,23 @@ const Register = () => {
         />
 
         <button type="submit" className="btn btn-block" disabled={isLoading}>
-          {isLoading?'Loading...':'submit'}
+          {isLoading ? 'Loading...' : 'submit'}
         </button>
-        {values.isMember?"Not a member yet ?":" Already a member ?"}
-        <button type="button" onClick={toggleMember} className="member-btn" >
-          {values.isMember?'Register':"login"}
+        <button
+          type="button"
+          className="btn btn-block btn-hipster"
+          disabled={isLoading}
+          onClick={() => {
+            dispatch(
+              loginUser({ email: 'testUser@test.com', password: 'secret' })
+            )
+          }}
+        >
+          {isLoading ? 'loading...' : 'demo'}
+        </button>
+        {values.isMember ? 'Not a member yet ?' : ' Already a member ?'}
+        <button type="button" onClick={toggleMember} className="member-btn">
+          {values.isMember ? 'Register' : 'login'}
         </button>
         {/* {console.log(values.isMember)} */}
       </form>
