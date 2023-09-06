137b76c32b13fcb8381307fea257ce251cfd54a5
Gyanendra <gyanen56@gmail.com>
2023-09-06 15:19:47 +1000

    showStats request in allJobsSlice

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -41,6 +41,20 @@ async(_, thunkAPI)=>{
     }
     
 })
+export const showStats = createAsyncThunk('allJobs/showStats', async(_, thunkAPI)=>{
+  let url = `/jobs/stats`
+  try {
+    const resp = await customFetch.get(url, {
+      headers: {
+        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+    })
+    console.log(resp.data)
+    
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg)
+  }
+})
 
 const allJobsSlice = createSlice({
   name: 'allJobs',
@@ -64,6 +78,18 @@ const allJobsSlice = createSlice({
     [getAllJobs.rejected]: (state, { payload }) => {
       state.isLoading = false
       toast.error(payload)
+    },
+    [showStats.pending]: (state) => {
+      state.isLoading = true
+    },
+    [showStats.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.stats = payload.defaultStats;
+      state.monthlyApplications = payload.monthlyApplications
+    },
+    [showStats.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
     },
   },
 })
