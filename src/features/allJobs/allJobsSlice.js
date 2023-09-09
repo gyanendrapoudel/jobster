aeb8698c128cd4e29498c417c8b64c475ff1379a
Gyanendra <gyanen56@gmail.com>
2023-09-09 17:18:12 +1000

    clearing state values after logout

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -1,32 +1,26 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import customFetch from "../../utils/axios"
-import { toast } from "react-toastify"
-import { getAllJobsThunk, showStatsThunk } from "./allJobsThunk"
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify'
+import { getAllJobsThunk, showStatsThunk } from './allJobsThunk'
 
-
-
-const initialFilterState ={
-    search:'',
-    searchStatus:'all',
-    searchType:'all',
-    sort:'latest',
-    sortOptions:['latest','oldest', 'a-z','z-a'],
-
+const initialFilterState = {
+  search: '',
+  searchStatus: 'all',
+  searchType: 'all',
+  sort: 'latest',
+  sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
 }
-const initialState={
-    
-    isLoading:true,
-    jobs:[],
-    totalJobs: 0,
-    numOfPages:1,
-    page:1, 
-    stats:{},
-    monthlyApplications:[],
-    ...initialFilterState,
-
+const initialState = {
+  isLoading: true,
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
+  stats: {},
+  monthlyApplications: [],
+  ...initialFilterState,
 }
-export const getAllJobs = createAsyncThunk('allJobs/getJobs',getAllJobsThunk);
-export const showStats = createAsyncThunk('allJobs/showStats', showStatsThunk);
+export const getAllJobs = createAsyncThunk('allJobs/getJobs', getAllJobsThunk)
+export const showStats = createAsyncThunk('allJobs/showStats', showStatsThunk)
 
 const allJobsSlice = createSlice({
   name: 'allJobs',
@@ -38,18 +32,18 @@ const allJobsSlice = createSlice({
     hideLoading: (state) => {
       state.isLoading = false
     },
-    handleChange:(state, {payload:{name, value}})=>{
+    handleChange: (state, { payload: { name, value } }) => {
       //state.page=1 later
-      state.page=1 // every new request this will set back page to 1
+      state.page = 1 // every new request this will set back page to 1
       state[name] = value
     },
-    clearFilters:(state)=>{
-      return{...state, ...initialFilterState}
+    clearFilters: (state) => {
+      return { ...state, ...initialFilterState }
     },
-    changePage:(state, {payload})=>{
-      state.page=payload
-     
-    }
+    changePage: (state, { payload }) => {
+      state.page = payload
+    },
+    clearAllJobsState: () => initialState,
   },
   extraReducers: {
     [getAllJobs.pending]: (state) => {
@@ -58,8 +52,8 @@ const allJobsSlice = createSlice({
     [getAllJobs.fulfilled]: (state, { payload }) => {
       state.isLoading = false
       state.jobs = payload.jobs
-      state.numOfPages=payload.numOfPages
-      state.totalJobs= payload.totalJobs
+      state.numOfPages = payload.numOfPages
+      state.totalJobs = payload.totalJobs
     },
     [getAllJobs.rejected]: (state, { payload }) => {
       state.isLoading = false
@@ -69,16 +63,23 @@ const allJobsSlice = createSlice({
       state.isLoading = true
     },
     [showStats.fulfilled]: (state, { payload }) => {
-    //  console.log(payload)
-      state.isLoading = false;
+      //  console.log(payload)
+      state.isLoading = false
       state.stats = payload.defaultStats
       state.monthlyApplications = payload.monthlyApplications
     },
     [showStats.rejected]: (state, { payload }) => {
-      state.isLoading = false;
-      toast.error(payload);
+      state.isLoading = false
+      toast.error(payload)
     },
   },
 })
-export const {showLoading, hideLoading, handleChange, clearFilters, changePage} =allJobsSlice.actions;
+export const {
+  showLoading,
+  hideLoading,
+  handleChange,
+  clearFilters,
+  changePage,
+  clearAllJobsState,
+} = allJobsSlice.actions
 export default allJobsSlice.reducer
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,6 +2,8 @@ import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 import { toast } from 'react-toastify'
 import customFetch from '../../utils/axios';
 import { addUserToLocalStorage, getUserFromLocalStorage, removeUserFromLocalStorage } from '../../utils/localStorage';
+import { clearAllJobsState } from '../allJobs/allJobsSlice'
+import { clearValues } from '../job/jobSlice'
 
 const initialState ={
     isLoading: false,
@@ -51,73 +53,89 @@ export const updateUser = createAsyncThunk('user/updateUser',
  }
 })
 
-const userSlice= createSlice({
-    name:'user',
-    initialState,
-    reducers:{
-      toggleSidebar:(state)=>{
-        state.isSidebarOpen=!state.isSidebarOpen;
-      },
-      logoutUser:(state,{payload})=>{
-        state.user = null;
-        state.isSidebarOpen=false;
-        removeUserFromLocalStorage();
-        if(payload){
-          toast.success(payload)
-        }
-
+export const clearStore = createAsyncThunk('user/clearStore',
+async(message, thunkAPI)=>{
+  try {
+    //logout user
+    thunkAPI.dispatch(logoutUser(message));
+    // clear jobs value
+    thunkAPI.dispatch(clearAllJobsState());
+    //clear job input values
+    thunkAPI.dispatch(clearValues());
+    return Promise.resolve();
+  } catch (error) {
+    return Promise.reject();
+  }
+}
+)
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    toggleSidebar: (state) => {
+      state.isSidebarOpen = !state.isSidebarOpen
+    },
+    logoutUser: (state, { payload }) => {
+      state.user = null
+      state.isSidebarOpen = false
+      removeUserFromLocalStorage()
+      if (payload) {
+        toast.success(payload)
       }
     },
-    extraReducers:{
-        [registerUser.pending]:(state)=>{
-            state.isLoading = true;
-        },
-        [registerUser.fulfilled]:(state,{payload})=>{
-          // in the payload we going to get whatever( resp.data) 
-        //   we returning from registerUser axios
-          const { user } = payload
-          state.isLoading = false
-          state.user = user
-          addUserToLocalStorage(user);
-          toast.success(`Hello There ${user.name}`)
-        },
-        [registerUser.rejected]:(state,{payload})=>{
-            state.isLoading = false;
-            toast.error(payload);
-        
+  },
+  extraReducers: {
+    [registerUser.pending]: (state) => {
+      state.isLoading = true
     },
-    
-        [loginUser.pending]:(state)=>{
-            state.isLoading = true;
-        },
-        [loginUser.fulfilled]:(state,{payload})=>{
-          // in the payload we going to get whatever( resp.data) 
-        //   we returning from loginUser axios
-          const { user } = payload
-          state.isLoading = false
-          state.user = user
-          addUserToLocalStorage(user)
-          toast.success(`Welcome back  ${user.name}`)
-        },
-        [loginUser.rejected]:(state,{payload})=>{
-            state.isLoading = false;
-            toast.error(payload);
-        },
-        [updateUser.pending]:(state)=>{
-            state.isLoading = true;
-        },
-        [updateUser.fulfilled]:(state,{payload})=>{
-          const { user } = payload
-          state.isLoading = false
-          state.user = user
-          addUserToLocalStorage(user);
-          toast.success(`User updated!`)
-        },
-        [updateUser.rejected]:(state,{payload})=>{
-            state.isLoading = false;
-            toast.error(payload);
-    }
-  }
-});
-export const {toggleSidebar,logoutUser}= userSlice.actions;
+    [registerUser.fulfilled]: (state, { payload }) => {
+      // in the payload we going to get whatever( resp.data)
+      //   we returning from registerUser axios
+      const { user } = payload
+      state.isLoading = false
+      state.user = user
+      addUserToLocalStorage(user)
+      toast.success(`Hello There ${user.name}`)
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      toast.error(payload)
+    },
+
+    [loginUser.pending]: (state) => {
+      state.isLoading = true
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      // in the payload we going to get whatever( resp.data)
+      //   we returning from loginUser axios
+      const { user } = payload
+      state.isLoading = false
+      state.user = user
+      addUserToLocalStorage(user)
+      toast.success(`Welcome back  ${user.name}`)
+    },
+    [loginUser.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      toast.error(payload)
+    },
+    [updateUser.pending]: (state) => {
+      state.isLoading = true
+    },
+    [updateUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload
+      state.isLoading = false
+      state.user = user
+      addUserToLocalStorage(user)
+      toast.success(`User updated!`)
+    },
+    [updateUser.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      toast.error(payload)
+    },
+    [clearStore.rejected]: () => {
+      toast.error('There was an error')
+    },
+  },
+})
+export const { toggleSidebar, logoutUser } = userSlice.actions
 export default userSlice.reducer;
